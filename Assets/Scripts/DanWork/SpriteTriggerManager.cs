ddda66ed311bc26667575b2f1c4e3d0cac721e85
Dan Miller <danmiller126407@gmail.com>
2019-01-27 10:09:53 -0800

    added colliders to controller anchors, added triggers to slap man and dog sprite

---
+++ Assets/Scripts/DanWork/SpriteTriggerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTriggerManager : MonoBehaviour {
+
+    const string k_ControllerTag = "Controller";
+
+    public enum InteractionType {Pet, Slap};
+    public InteractionType interaction;
+
+    PlayerGameController m_playerGameController;
+
+    private void OnEnable()
+    {
+        m_playerGameController = FindObjectOfType<PlayerGameController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag(k_ControllerTag))
+        {
+            switch(interaction)
+            {
+                case InteractionType.Pet:
+                    m_playerGameController.HandleGesture(interaction.ToString());
+                break;
+
+                case InteractionType.Slap:
+                    m_playerGameController.HandleGesture(interaction.ToString());
+                break;
+            }
+        }
+    }
+}
