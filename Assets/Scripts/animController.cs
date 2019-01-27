83287d1f5812785737574348e5bcb175fa1a9701
Niall Jones <jones.niall@gmail.com>
2019-01-27 13:01:47 -0800

    Niall's changes (bathroom scene)

---
+++ Assets/Scripts/animController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animController : MonoBehaviour {
+
+    public Animator anim;
+    public AudioClip ToiletLidOpenClip;
+    public AudioSource TLOSource;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        TLOSource.clip = ToiletLidOpenClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown("1"))
+        {
+            anim.Play("animLid");
+            TLOSource.Play();
+        }
+    }
+}
