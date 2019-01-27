fc78f1bc9f923acc387731306b38f3cd5bd41f27
Niall Jones <jones.niall@gmail.com>
2019-01-27 10:30:07 -0800

    Rigged trump head and change furniture

---
+++ Assets/trumpHeadController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class trumpHeadController : MonoBehaviour {
+
+    public GameObject headSprite;
+
+    // Use this for initialization
+    void Start () {
+        VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        headSprite.transform.position = VRTK_DeviceFinder.HeadsetTransform().position;
+        headSprite.transform.localRotation = VRTK_DeviceFinder.HeadsetTransform().rotation;
+    }
+}
