aaf368434e610ce2163bf931c932f07ff730aba5
Shane <scaler0222@gmail.com>
2019-01-27 03:49:54 -0800

    Added VRTK to the StreetScene, however the player still falls through the map due to no colliders on the roads. (TODO: consider occlusion culling if performance becomes an issue with the map size)

---
+++ Assets/ReactToTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactToTouch : MonoBehaviour
+{
+    PlayerGameController pgc;
+
+    void Start()
+    {
+        pgc = GameObject.FindObjectOfType<PlayerGameController>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch (tag)
+        {
+            case "Slap":
+                pgc.HandleGesture("Slap");
+                transform.parent.Translate(Vector3.forward);
+                Debug.Log("Slapped!");
+                break;
+            case "Pet":
+                pgc.HandleGesture("Pet");
+                Debug.Log("Pet!");
+                break;
+            default:
+                Debug.Log("In default, what did you do wrong!?");
+                return;
+        }
+        Destroy(transform.parent.gameObject, 3f);
+    }
+}
