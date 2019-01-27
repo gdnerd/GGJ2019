317a9019bbdb0558f9bf1df95d2a75cfe5d2efcb
Shane <scaler0222@gmail.com>
2019-01-26 17:54:38 -0800

    Added VRTK setup prefabs folder, basic gas station scene with 3 food items and voice recognition logic

---
+++ Assets/Scenes/GasStationScene/EatFood.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EatFood : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        print(gameObject + " collided with " + other.gameObject);
+
+        if (other.tag != "Hand")
+        {
+            // Check tags and load scene depending on type of food eaten.
+            // Done asynchronously so that the player can simply leave the
+            // start zone without having to wait for a loading screen.
+            // Depending on how we set up difficulty levels, it might be easier
+            // to just alter values on the fly
+            switch (gameObject.tag)
+            {
+                case "Easy":
+                    StartCoroutine(LoadAsyncScene("Easy"));
+                    break;
+                case "Medium":
+                    StartCoroutine(LoadAsyncScene("Medium"));
+                    break;
+                case "Hard":
+                    StartCoroutine(LoadAsyncScene("Hard"));
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+
+    IEnumerator LoadAsyncScene(string sceneName)
+    {
+        print("Loading scene: " + sceneName);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/VoiceRecognizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class VoiceRecognizer : MonoBehaviour {
+
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<String, Action>();
+
+	// Use this for initialization
+	void Start () {
+        actions.Add("Test", Test);
+        actions.Add("Hello", Hello);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+    }
+
+    private void OnApplicationQuit()
+    {
+        keywordRecognizer.Stop();
+    }
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    void Test()
+    {
+        print("Test");
+    }
+
+    void Hello()
+    {
+        print("Hello");
+    }
+}
+ 
